ae3beaa0226091d8159a74926ebc4a6a0954e97a
Mike Bostock <mbostock@gmail.com>
2013-03-13 09:33:53 -0700

    Tests for circular and self imports.

--- test/smash-readAllImports-test.js
+++ test/smash-readAllImports-test.js
@@ -5,36 +5,116 @@ var vows = require("vows"),
 var suite = vows.describe("smash.readAllImports");
 
 suite.addBatch({
-  "on a file with no imports": {
-    topic: function() {
-      smash.readAllImports(["test/foo.js"], this.callback);
+  "readAllImports": {
+    "on a file with no imports": {
+      topic: function() {
+        smash.readAllImports(["test/foo.js"], this.callback);
+      },
+      "returns only the input file": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js"]);
+      }
     },
-    "returns only the input file": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js"]);
-    }
-  },
-  "on a file with one import": {
-    topic: function() {
-      smash.readAllImports(["test/imports-foo.js"], this.callback);
+    "on a file with imports with trailing comments": {
+      topic: function() {
+        smash.readAllImports(["test/trailing-comment-import.js"], this.callback);
+      },
+      "returns the empty array": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/trailing-comment-import.js"]);
+      }
     },
-    "returns the expected import followed by the input file": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/imports-foo.js"]);
-    }
-  },
-  "on a file with multiple imports": {
-    topic: function() {
-      smash.readAllImports(["test/imports-foo-bar-baz.js"], this.callback);
+    "on a file with invalid import syntax": {
+      topic: function() {
+        var callback = this.callback;
+        smash.readAllImports(["test/invalid-import-syntax.js"], function(error) {
+          callback(null, error);
+        });
+      },
+      "throws an error with the expected message": function(error) {
+        assert.deepEqual(error.message, "invalid import: test/invalid-import-syntax.js:0: import foo;");
+      }
     },
-    "returns the expected imports, in order": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/baz.js", "test/imports-foo-bar-baz.js"]);
-    }
-  },
-  "on a file with nested imports": {
-    topic: function() {
-      smash.readAllImports(["test/imports-imports-foo.js"], this.callback);
+    "on a file with that imports a file that does not exist": {
+      topic: function() {
+        var callback = this.callback;
+        smash.readAllImports(["test/imports-not-found.js"], function(error) {
+          callback(null, error);
+        });
+      },
+      "throws an error with the expected message": function(error) {
+        assert.deepEqual(error.message, "ENOENT, open 'test/not-found.js'");
+      }
     },
-    "returns the expected imports, in order": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/imports-foo.js", "test/imports-imports-foo.js"]);
+    "on a file with a commented-out import": {
+      topic: function() {
+        smash.readAllImports(["test/commented-import.js"], this.callback);
+      },
+      "ignores the commented-out input": function(imports) {
+        assert.deepEqual(imports, ["test/commented-import.js"]);
+      }
+    },
+    "on a file with a not-commented-out import": {
+      topic: function() {
+        smash.readAllImports(["test/not-commented-import.js"], this.callback);
+      },
+      "does not ignore the not-commented-out import": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/not-commented-import.js"]);
+      }
+    },
+    "on a file with one import": {
+      topic: function() {
+        smash.readAllImports(["test/imports-foo.js"], this.callback);
+      },
+      "returns the expected import followed by the input file": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/imports-foo.js"]);
+      }
+    },
+    "on a file with multiple imports": {
+      topic: function() {
+        smash.readAllImports(["test/imports-foo-bar-baz.js"], this.callback);
+      },
+      "returns the imports in order of declaration": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/baz.js", "test/imports-foo-bar-baz.js"]);
+      }
+    },
+    "on a file with nested imports": {
+      topic: function() {
+        smash.readAllImports(["test/imports-imports-foo.js"], this.callback);
+      },
+      "returns the imports in order of dependency": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/imports-foo.js", "test/imports-imports-foo.js"]);
+      }
+    },
+    "on multiple input files": {
+      topic: function() {
+        smash.readAllImports(["test/foo.js", "test/bar.js", "test/baz.js"], this.callback);
+      },
+      "returns the expected imports, in order": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/baz.js"]);
+      }
+    },
+    "with redundant input files": {
+      topic: function() {
+        smash.readAllImports(["test/foo.js", "test/foo.js"], this.callback);
+      },
+      "ignores the redundant imports": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js"]);
+      }
+    },
+    "when a file that imports itself": {
+      topic: function() {
+        smash.readAllImports(["test/imports-self.js"], this.callback);
+      },
+      "the self-import has no effect": function(imports) {
+        assert.deepEqual(imports, ["test/imports-self.js"]);
+      }
+    },
+    "when circular imports are encountered": {
+      topic: function() {
+        smash.readAllImports(["test/imports-circular-foo.js"], this.callback);
+      },
+      "imports are returned in arbtirary order": function(imports) {
+        assert.deepEqual(imports, ["test/imports-circular-bar.js", "test/imports-circular-foo.js"]);
+      }
     }
   }
 });
--- test/smash-readImports-test.js
+++ test/smash-readImports-test.js
@@ -5,79 +5,97 @@ var vows = require("vows"),
 var suite = vows.describe("smash.readImports");
 
 suite.addBatch({
-  "on a file with no imports": {
-    topic: function() {
-      smash.readImports("test/foo.js", this.callback);
+  "readImports": {
+    "on a file with no imports": {
+      topic: function() {
+        smash.readImports("test/foo.js", this.callback);
+      },
+      "returns the empty array": function(imports) {
+        assert.deepEqual(imports, []);
+      }
     },
-    "returns the empty array": function(imports) {
-      assert.deepEqual(imports, []);
-    }
-  },
-  "on a file with imports with trailing comments": {
-    topic: function() {
-      smash.readImports("test/trailing-comment-import.js", this.callback);
+    "on a file with imports with trailing comments": {
+      topic: function() {
+        smash.readImports("test/trailing-comment-import.js", this.callback);
+      },
+      "returns the empty array": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/bar.js"]);
+      }
     },
-    "returns the empty array": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/bar.js"]);
-    }
-  },
-  "on a file with an invalid import": {
-    topic: function() {
-      var callback = this.callback;
-      smash.readImports("test/invalid-import.js", function(error) {
-        callback(null, error);
-      });
+    "on a file with invalid import syntax": {
+      topic: function() {
+        var callback = this.callback;
+        smash.readImports("test/invalid-import-syntax.js", function(error) {
+          callback(null, error);
+        });
+      },
+      "throws an error with the expected message": function(error) {
+        assert.deepEqual(error.message, "invalid import: test/invalid-import-syntax.js:0: import foo;");
+      }
     },
-    "throws an error with the expected message": function(error) {
-      assert.deepEqual(error.message, "invalid import: test/invalid-import.js:0: import foo;");
-    }
-  },
-  "on a file with a commented-out import": {
-    topic: function() {
-      smash.readImports("test/commented-import.js", this.callback);
+    "on a file with that imports a file that does not exist": {
+      topic: function() {
+        smash.readImports("test/imports-not-found.js", this.callback);
+      },
+      "returns the expected import": function(imports) {
+        assert.deepEqual(imports, ["test/not-found.js"]);
+      }
     },
-    "returns the empty array": function(imports) {
-      assert.deepEqual(imports, []);
-    }
-  },
-  "on a file with a not commented-out import": {
-    topic: function() {
-      smash.readImports("test/not-commented-import.js", this.callback);
+    "on a file with a commented-out import": {
+      topic: function() {
+        smash.readImports("test/commented-import.js", this.callback);
+      },
+      "ignores the commented-out input": function(imports) {
+        assert.deepEqual(imports, []);
+      }
     },
-    "returns the empty array": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js"]);
-    }
-  },
-  "on a file with one import": {
-    topic: function() {
-      smash.readImports("test/imports-foo.js", this.callback);
+    "on a file with a not-commented-out import": {
+      topic: function() {
+        smash.readImports("test/not-commented-import.js", this.callback);
+      },
+      "does not ignore the not-commented-out import": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js"]);
+      }
     },
-    "returns the expected import": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js"]);
-    }
-  },
-  "on a file with multiple imports": {
-    topic: function() {
-      smash.readImports("test/imports-foo-bar-baz.js", this.callback);
+    "on a file with one import": {
+      topic: function() {
+        smash.readImports("test/imports-foo.js", this.callback);
+      },
+      "returns the expected import": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js"]);
+      }
     },
-    "returns the expected imports, in order": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/baz.js"]);
-    }
-  },
-  "on a file with multiple redundant imports": {
-    topic: function() {
-      smash.readImports("test/imports-foo-foo-bar-foo.js", this.callback);
+    "on a file with multiple imports": {
+      topic: function() {
+        smash.readImports("test/imports-foo-bar-baz.js", this.callback);
+      },
+      "returns the expected imports, in order": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/bar.js", "test/baz.js"]);
+      }
     },
-    "returns all imports, in order": function(imports) {
-      assert.deepEqual(imports, ["test/foo.js", "test/foo.js", "test/bar.js", "test/foo.js"]);
-    }
-  },
-  "on a file with nested imports": {
-    topic: function() {
-      smash.readImports("test/imports-imports-foo.js", this.callback);
+    "on a file with multiple redundant imports": {
+      topic: function() {
+        smash.readImports("test/imports-foo-foo-bar-foo.js", this.callback);
+      },
+      "returns all imports, in order": function(imports) {
+        assert.deepEqual(imports, ["test/foo.js", "test/foo.js", "test/bar.js", "test/foo.js"]);
+      }
     },
-    "returns the expected imports, in order": function(imports) {
-      assert.deepEqual(imports, ["test/imports-foo.js"]);
+    "on a file with nested imports": {
+      topic: function() {
+        smash.readImports("test/imports-imports-foo.js", this.callback);
+      },
+      "returns the expected imports, in order": function(imports) {
+        assert.deepEqual(imports, ["test/imports-foo.js"]);
+      }
+    },
+    "on a file that imports itself": {
+      topic: function() {
+        smash.readImports("test/imports-self.js", this.callback);
+      },
+      "returns the expected import": function(imports) {
+        assert.deepEqual(imports, ["test/imports-self.js"]);
+      }
     }
   }
 });
