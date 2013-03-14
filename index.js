2c5f7eeed2afd436b1a183a8a1d04743254cec9d
Mike Bostock <mbostock@gmail.com>
2013-03-13 20:41:35 -0700

    Optional sandbox for load.

--- index.js
+++ index.js
@@ -59,12 +59,18 @@ function smash(files) {
 
 // Loads the specified files and their imports, then evaluates the specified
 // expression in the context of the concatenated code.
-smash.load = function(files, expression, callback) {
+smash.load = function(files, expression, sandbox, callback) {
+  if (arguments.length < 4) callback = sandbox, sandbox = null;
   var chunks = [];
   smash(files)
       .on("error", callback)
       .on("data", function(chunk) { chunks.push(chunk); })
-      .on("end", function() { callback(null, vm.runInNewContext(chunks.join("") + ";" + expression)); });
+      .on("end", function() {
+        var error, result;
+        try { result = vm.runInNewContext(chunks.join("") + ";" + expression, sandbox); }
+        catch (e) { error = e; }
+        callback(error, result);
+      });
 };
 
 // Returns the network of imports, starting with the specified input files.
